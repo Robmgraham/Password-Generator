a027095a3f97fa6017029d04282b9a8e5631e8f2
rancid <robmgraham13@yahoo.com>
2020-07-19 21:35:23 -0700

    added comments to seperate prompts

--- script.js
+++ script.js
@@ -7,54 +7,63 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 
 var capitalCase =("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z");
 var lowerCase = ("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z");
 var specialChar = ("!,#,$,%,&,',(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,\,],^,_,`,{,|,},~");
 var numbers = ("1,2,3,4,5,6,7,8,9,0");
 var charTotal;
-  
+
+
+
+        //-------while loop keeps characters between 8-128-------//
 confirm ("To begin, let's set some parameters for your password")
-
-
-while (true) {
+    while (true) {
     charTotal = prompt("How long would you like your password", "");
     if (charTotal >= 8 && charTotal <= 128) {
         break;
     }
     alert("It should be between 8 and 128 charcters in length");
-}
-alert("Now lets choose its contents");
-     
-    
+    }                 //------------------end loop---------------------//
+
+
+    alert("Now lets choose its contents");
+         
+                 //---------------Capital Case prompt-------------//
      confirm ("Would you like  to include CAPTIAL CASE letters")
-       if (true) {const randomElement = capitalCase[Math.floor(Math.random()* 8)];
+        if (true) {const randomElement = capitalCase[Math.floor(Math.random()* 2)];
            
        } else {
            
        }
+
+                 //---------------Lower Case prompt-------------//
     confirm("Would you like to include lower case letters")
-       if (true) {const randomElement = lowerCase[Math.floor(Math.random()* 8)];
+        if (true) {const randomElement = lowerCase[Math.floor(Math.random()* 2)];
            
        } else {
            
        }
+
+                //---------------Special Characters prompt------------------//
     confirm("would you like to include special characters")
-        if (true) {const randomElement = specialChar[Math.floor(Math.random()* 8)];
+        if (true) {const randomElement = specialChar[Math.floor(Math.random()* 2)];
             
         } else {
             
         }
+
+                    //---------------Numbers prompt-------------//
     confirm("would you like to include numbers");
-    if (true) {const randomElement = numbers[Math.floor(Math.random()* 8)];
+        if (true) {const randomElement = numbers[Math.floor(Math.random()* 2)];
         
-    } else {
+        } else {
         
-    }
+        }
+    
+    
 
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+    

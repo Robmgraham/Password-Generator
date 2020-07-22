8b4cd0a0cfe3e90a726276cc266e9cc638c598fe
rancid <robmgraham13@yahoo.com>
2020-07-22 00:29:33 -0700

    tried to fix syntax error

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-    return ("password");
+    return (passwordCapital+passwordNumbers+passwordSpecial+passwordlower);
 }
 
 // Write password to the #password input
@@ -13,16 +13,16 @@ function writePassword() {
   passwordText.value = password;
 }
 
-var lowerCase =["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
-var capitalCase = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
-var specialChar = ["!,#,$,%,&,',(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,\,],^,_,`,{,|,},~"];
-var numbers = ["1,2,3,4,5,6,7,8,9,0"];
+var lowerCase ="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+var capitalCase = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+var specialChar = "!,#,$,%,&,',(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,\,],^,_,`,{,|,},~";
+var numbers = "1,2,3,4,5,6,7,8,9,0";
 var charTotal;
 
-var splitCapitalCase = split(capitalCase, ",");
-var splitLowerCase = split(lowerCase, ",");
-var splitSpecialCahr = split(specialChar, ",");
-var splitNumbers = split( ",");
+var splitCapitalCase = split (capitalCase, ",");
+var splitLowerCase = split (lowerCase, ",");
+var splitSpecialCahr = split (specialChar, ",");
+var splitNumbers = split (numbers, ",");
 
         //-------while loop keeps characters between 8-128-------//
 
@@ -30,7 +30,7 @@ var splitNumbers = split( ",");
         alert ("To begin, let's set some parameters for your password")
         while (true) {
         charTotal = prompt("How long would you like your password", "");
-        if (charTotal >= 8 && charTotal <= 128) {
+        if (charTotal >= 8 && charTotal <= 128) 
             break;
         }
         alert("It should be between 8 and 128 charcters in length");
@@ -53,19 +53,19 @@ var splitNumbers = split( ",");
     var hasLower = confirm("Would you like to include lower case letters")
         if (hasLower) {passwordlower = splitLowerCase[Math.floor(Math.random()* charTotal)];
            
-      
+        }
 
                 //---------------Special Characters prompt------------------//
     var hasSpecial = confirm("would you like to include special characters")
         if (hasSpecial) { passwordSpecial = splitSpecialChar[Math.floor(Math.random()* charTotal)];
             
-        
+        }
 
                     //---------------Numbers prompt-------------//
     var hasNumbers = confirm("would you like to include numbers");
         if (hasNumbers) { passwordNumbers = splitNumbers[Math.floor(Math.random()* charTotal)];
        
-    
+        }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
